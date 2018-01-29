e996d38061fc982ac516122cfaff68f21ea99ed6
Exulansis <rusu.eugeniu@gmx.de>
2018-01-29 23:35:08 +0100

    Added javascript wrappers and introduced js tests.

---
+++ tests/ethereumResolver.test.ts
@@ -0,0 +1,42 @@
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+
+import testData from './data/testData'
+
+import EthereumResolver from '../ts/ethereumResolver'
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+
+describe('Ethereum Resolver', () => {
+  const ethResolver = new EthereumResolver()
+  it('Should correctly register a user\'s DDO hash', async () => {
+    const hash = await ethResolver.updateDIDRecord(
+      testData.firstTestAccount, testData.testUserDID,
+      testData.mockDDOHash
+    )
+
+    expect(await ethResolver.resolveDID(testData.testUserDID))
+      .to.equal(testData.mockDDOHash)
+  })
+
+  it('Should return error in case writting record fails', async () => {
+    await expect(ethResolver.updateDIDRecord(
+      testData.secondTestAcount,
+      testData.testUserDID,
+      testData.mockDDOHash
+    )).to.be.rejectedWith(
+      'Returned error: VM Exception while processing transaction: revert'
+    )
+  })
+
+  it('Should correctly query contract for the user\'s DDO hash', async () => {
+    const hash = await ethResolver.resolveDID(testData.testUserDID)
+    expect(hash).to.equal(testData.mockDDOHash)
+  })
+
+  it('Should return error in case reading record fails', async () => {
+    await expect(ethResolver.resolveDID('invalidInput')).to.be.rejected
+  })
+})
