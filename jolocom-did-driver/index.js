f8e1851b8a9571235fcb272b2bd9aabe78073f1c
Charles Cunningham <charles.cunningham@outlook.com>
2019-01-17 19:57:27 +0100

    huge simplifications

--- jolocom-did-driver/index.js
+++ jolocom-did-driver/index.js
@@ -1,39 +1,24 @@
-const EthResolver = require('jolocom-registry-contract')
-const IPFS = require('ipfs-mini')
+const JolocomResolver = require('jolocom-lib').JolocomLib.registries.jolocom
 
 const express = require('express')
 const driver = express()
 
-const ethResolverURL = process.env.ETH_PROVIDER
-const contractAddress = '0xd4351c3f383d79ba378ed1875275b1e7b960f120'
-const ipfsHost = process.env.IPFS_HOST
-const ipfsPort = 5001
 const port = 8080
 
+const JR = JolocomResolver.create()
 
-const jolocomEthereumResolver = new EthResolver({
-    providerUrl: ethResolverURL,
-    contractAddress: '0xd4351c3f383d79ba378ed1875275b1e7b960f120'
-})
-
-const ipfs = new IPFS({ host: ipfsHost, port: ipfsPort, protocol: 'https' });
-
-driver.get('/1.0/identifiers/*', function (req, res) {
-    const url = req.url
-    const regex = /\/1.0\/identifiers\/(did:.*)/
-    const did = regex.exec(url)[1]
+driver.get('/1.0/identifiers/:did', function (req, res) {
+    const did = req.params.did
 
     console.log(did)
 
-    const ddoHash = jolocomEthereumResolver.resolveDID(did)
-
-    ipfs.cat(ddoHash, function(err, result) {
-        if (err) {
-            res.send(err)
-        } else {
-            res.send(result)
-        }
+    JR.resolve(did).then(function(identity) {
+        console.log(identity.didDocument.toJSON())
+        res.send(identity.didDocument.toJSON())
+    }).catch(function(reason) {
+        res.send()
     })
+
 })
 
 var server = driver.listen(port, function () {
