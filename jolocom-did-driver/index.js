482a982350b9284b0eb09af5ef43a2c1bcc866ee
Charles Cunningham <charles.cunningham@outlook.com>
2019-01-17 17:45:38 +0100

    added environment variables

--- jolocom-did-driver/index.js
+++ jolocom-did-driver/index.js
@@ -4,12 +4,19 @@ const IPFS = require('ipfs-mini')
 const express = require('express')
 const driver = express()
 
+const ethResolverURL = process.env.ETH_PROVIDER
+const contractAddress = '0xd4351c3f383d79ba378ed1875275b1e7b960f120'
+const ipfsHost = process.env.IPFS_HOST
+const ipfsPort = 5001
+const port = 8080
+
+
 const jolocomEthereumResolver = new EthResolver({
-    providerUrl: 'https://rinkeby.infura.io/',
+    providerUrl: ethResolverURL,
     contractAddress: '0xd4351c3f383d79ba378ed1875275b1e7b960f120'
 })
 
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const ipfs = new IPFS({ host: ipfsHost, port: ipfsPort, protocol: 'https' });
 
 driver.get('/1.0/identifiers/*', function (req, res) {
     const url = req.url
@@ -29,6 +36,6 @@ driver.get('/1.0/identifiers/*', function (req, res) {
     })
 })
 
-var server = driver.listen(8081, function () {
-    console.log("Jolocom Resolver driver active on port 8081...")
+var server = driver.listen(port, function () {
+    console.log("Jolocom Resolver driver active on port 8080...")
 })
