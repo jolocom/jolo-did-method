52b184efef4434b9ad24cd38ed6f083de26ab977
Charles Cunningham <charles.cunningham@outlook.com>
2019-01-17 17:35:40 +0100

    added ipfs fetching

--- jolocom-did-driver/index.js
+++ jolocom-did-driver/index.js
@@ -1,12 +1,15 @@
 const EthResolver = require('jolocom-registry-contract')
+const IPFS = require('ipfs-mini')
 
 const express = require('express')
 const driver = express()
 
-export const jolocomEthereumResolver = new EthResolver({
+const jolocomEthereumResolver = new EthResolver({
     providerUrl: 'https://rinkeby.infura.io/',
     contractAddress: '0xd4351c3f383d79ba378ed1875275b1e7b960f120'
 })
+
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
 driver.get('/1.0/identifiers/*', function (req, res) {
     const url = req.url
@@ -15,8 +18,14 @@ driver.get('/1.0/identifiers/*', function (req, res) {
 
     console.log(did)
 
-    jolocomEthereumResolver.resolveDID(did).then((doc) => {
-      res.send(doc)
+    const ddoHash = jolocomEthereumResolver.resolveDID(did)
+
+    ipfs.cat(ddoHash, function(err, result) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
     })
 })
 
