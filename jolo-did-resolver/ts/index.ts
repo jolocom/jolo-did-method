49fab4477db1e79e76f14322d2a26b090f2087c1
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-01-22 17:19:06 +0100

    [jolo-did-resolver] add infura api key

--- jolo-did-resolver/ts/index.ts
+++ jolo-did-resolver/ts/index.ts
@@ -3,7 +3,7 @@ import RegistryContract from "jolocom-registry-contract";
 import { IpfsStorageAgent } from "./ipfs";
 
 const CONTRACT_ADDRESS = '0xd4351c3f383d79ba378ed1875275b1e7b960f120';
-const PROVIDER_URI = 'https://rinkeby.infura.io/';
+const PROVIDER_URI = 'https://rinkeby.infura.io/v3/64fa85ca0b28483ea90919a83630d5d8';
 const IPFS_ENDPOINT = 'https://ipfs.jolocom.com:443'
 
 export function getResolver(providerUri: string = PROVIDER_URI, contractAddress: string = CONTRACT_ADDRESS, ipfsHost: string = IPFS_ENDPOINT) {
