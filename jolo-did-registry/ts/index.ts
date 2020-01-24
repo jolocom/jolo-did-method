f338300b9fbcf2729847f67b0772b7c9c9919b84
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-01-24 11:50:40 +0100

    [jolo-did-registry] update infura endpoint to v3

--- jolo-did-registry/ts/index.ts
+++ jolo-did-registry/ts/index.ts
@@ -3,7 +3,7 @@ import { IpfsStorageAgent } from "./ipfs";
 import { IDidDocument } from "@decentralized-identity/did-common-typescript"
 
 const JOLOCOM_PUBLIC_PROFILE_TYPE = "JolocomPublicProfile"
-export const infura = 'https://rinkeby.infura.io/'
+export const infura =  'https://rinkeby.infura.io/v3/64fa85ca0b28483ea90919a83630d5d8'
 export const jolocomContract = '0xd4351c3f383d79ba378ed1875275b1e7b960f120'
 export const jolocomIpfsHost = 'https://ipfs.jolocom.com:443'
 
