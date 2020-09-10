b5dcb308915000f24f6ac3a4a8a118da9b97caae
Exulansis <rusu.eugeniu@gmx.de>
2020-09-10 16:48:25 +0200

    docs(did-driver): Updated README

--- packages/jolocom-did-driver/index.ts
+++ packages/jolocom-did-driver/index.ts
@@ -5,6 +5,7 @@ import { Resolver } from "did-resolver";
 const PORT = 8080;
 
 const app = express();
+
 app.get('/1.0/identifiers/:did', async (req, res) => {
   const did = req.params.did;
   console.log(did);
